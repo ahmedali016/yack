0eac0adbc2ae9e65c661d0be82fb3aca85186936
ahmedali016 <42156503+ahmedali016@users.noreply.github.com>
2019-10-11 14:56:02 +0200

    all files
    all files

---
+++ yackeenTask/yackeenTask/ViewModels/viewArticls.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace yackeenTask.ViewModels
+{
+    public class viewArticls
+    {
+        public int articlid { set; get; }
+        public string title { set; get; }
+        public string article { set; get; }
+        public DateTime articleDate { set; get; }
+
+        public List<viewCategory> categ{ set; get; }
+}
+}
